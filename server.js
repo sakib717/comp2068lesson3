3bf7304c5b07048b834df7771fe1b8e1f3844a11
sakib717 <sakibsabbir7@gmail.com>
2016-03-11 02:16:31 -0500

    updated server.js to run express

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 "use strict";
 /// <reference path= "_reference.ts" />
-var http = require('http');
+var express = require('express');
+var app = express();
 var port = process.env.port || 3000;
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//Main Route
+app.get('/', function (req, res, next) {
+    res.send('Hello Express!');
+});
+//Info Route
+app.get('/info', function (req, res) {
+    res.send('Your custom info page right here');
+});
+app.listen(port, function () {
+    console.log("App Server Started...on port: " + port);
+});
+/*
+import http = require('http');
+
+var port:number = process.env.port || 3000;
+var server:http.Server = http.createServer(
+    function(req:http.ServerRequest, res:http.ServerResponse){
+    res.writeHead(200,{'Content-Type': 'text/plain'});
     res.end("Hello Node!");
 });
-server.listen(port, function () {
+
+server.listen(port, function(){
     console.log("Server Started...Listening on port: " + port);
-});
+    
+}); */ 
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -1,4 +1,45 @@
 /// <reference path= "_reference.ts" />
+import express = require('express');
+
+
+var app:express.Express = express();
+
+var port:number = process.env.port || 3000;
+
+//Main Route
+app.get('/', function (req:express.Request, res:express.Response, next:any) {
+  res.send('Hello Express!');
+});
+
+//Info Route
+app.get('/info', function(req:express.Request, res:express.Response) {
+   res.send('Your custom info page right here'); 
+});
+
+app.listen(port, function(){
+    console.log("App Server Started...on port: " + port);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 import http = require('http');
 
 var port:number = process.env.port || 3000;
@@ -11,4 +52,4 @@ var server:http.Server = http.createServer(
 server.listen(port, function(){
     console.log("Server Started...Listening on port: " + port);
     
-});
+}); */
