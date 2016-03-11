b50132f6b8be7b10c192b2bb51a2b14d26fdf7d5
sakib717 <sakibsabbir7@gmail.com>
2016-03-11 01:17:30 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+"use strict";
+/// <reference path= "_reference.ts" />
+var http = require('http');
+var port = process.env.port || 3000;
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("Hello Node!");
+});
+server.listen(port, function () {
+    console.log("Server Started...Listening on port: " + port);
+});
+//# sourceMappingURL=server.js.map
